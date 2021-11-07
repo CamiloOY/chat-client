db58c9da44fa60a3e498b73960817d48b3df19ee
CamiloOY <amiloc333@gmail.com>
2021-11-07 20:46:24 +0000

    Added style for when you've been tagged in a message

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,8 +3,9 @@ import React from "react";
 export default function Message(props) {
 	let message = props.children;
 	const parts=message.split(/(@[a-zA-Z0-9]+)/g);
+	let me_tagged = message.match(new RegExp(`@${props.me}($|\\s)`));
 	return (
-		<li className="py-3 bg-gray-50 px-2 hover:bg-gray-100">
+		<li className={"py-3 px-2 " + (me_tagged ? " bg-blue-50 border-l-2 border-blue-500 hover:bg-blue-100" : "bg-gray-50 hover:bg-gray-100")}>
 			<div className="font-bold text-sm">{props.sender}</div>
 			{
 				parts.map(item => {
