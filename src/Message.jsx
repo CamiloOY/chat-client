0d4f2acd40c8f27e50460f766099cd7ed3bce2cb
CamiloOY <amiloc333@gmail.com>
2021-11-07 20:20:54 +0000

    Improved styling of mentions

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,14 +3,13 @@ import React from "react";
 export default function Message(props) {
 	let message = props.children;
 	const parts=message.split(/(@[a-zA-Z0-9]+)/g);
-	console.log("parts",parts);
 	return (
 		<li className="py-3 bg-gray-50 px-2 hover:bg-gray-100">
 			<div className="font-bold text-sm">{props.sender}</div>
 			{
 				parts.map(item => {
 					if(item[0]==="@") {
-						return <span key={item+Date.now()} className="bg-yellow-300">{item}</span>
+						return <span key={item+Date.now()} className="bg-yellow-200 px-1 rounded-md font-medium hover:bg-yellow-300 transition-colors">{item}</span>
 					}
 					else {
 						return item;
